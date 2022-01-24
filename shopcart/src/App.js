33a72e473389af653cd0c1f864b934dcaaad4137
Qi Zhang <zq170829@gmail.com>
2022-01-23 23:26:13 -0600

    functionalities setup

--- shopcart/src/App.js
+++ shopcart/src/App.js
@@ -3,72 +3,106 @@ import "./styles.css";
 import { ListGroup, ListGroupItem } from "reactstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React, { Component } from "react";
-import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import {
+  faShoppingCart,
+  faPlusCircle,
+  faMinusCircle,
+  faRegistered,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { products } from "./products";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      heading: "Shop to React",
-      items: [
-        {
-          id: 0,
-          name: "Unisex Cologne",
-          picture: "products/cologne.jpg",
-          quantity: 0,
-        },
-        {
-          id: 1,
-          name: "Apple iWatch",
-          picture: "products/iwatch.jpg",
-          quantity: 0,
-        },
-        {
-          id: 2,
-          name: "Unique Mug",
-          picture: "products/mug.jpg",
-          quantity: 0,
-        },
-        {
-          id: 3,
-          name: "Mens Wallet",
-          picture: "products/wallet.jpg",
-          quantity: 0,
-        },
-      ],
+      items: products,
     };
   }
-  
+
+  increaseBtn = (addvalue) => {
+    if (addvalue.value < 10) {
+      const updateValue = addvalue.value++;
+      this.setState({ updateValue });
+    }
+  };
+
+  decreaseBtn = (subvalue) => {
+    if (subvalue.value > 0) {
+      const updateValue = subvalue.value--;
+      this.setState({ updateValue });
+    }
+  };
+
   render() {
-    const cartCountTotal = this.state.items.reduce((a, b) => a + b.quantity, 0);
     return (
       <div class="shop_cart">
-        <div className="title">
-          <h1>{this.state.heading}</h1>
-          <div className="total">
-            <FontAwesomeIcon icon={faShoppingCart} />
-            <span className="">{cartCountTotal} Items</span>
-          </div>
-        </div>
-        {this.state.items.map((item) => (
-          <ListGroup className="shop_item" key={item.id}>
-            <ListGroupItem>
-              <h5 className="shop_name">{item.name}</h5>
-              <img
-                className="shop_picture"
-                src={item.picture}
-                width="180"
-                height="180"
-                alt="shopping item"
-              />
-              <input type="number" className="shop_quantity" min="0" placeholder={item.quantity}/> quantity
-            </ListGroupItem>
-          </ListGroup>
-        ))}
+        <Nav
+          ItemCount={this.state.items
+            .map((prod) => prod.value)
+            .reduce((acc, curr, index) => acc + curr, 0)}
+        />
+        <DisplayProducts
+          prods={this.state.items}
+          increaseBtn={this.increaseBtn}
+          decreaseBtn={this.decreaseBtn}
+        />
       </div>
     );
   }
+}
+
+function Nav(props) {
+  return (
+    <div className="navbar p-5 bg-info">
+      <h1>
+      <span className="px-2">Shop 2 </span>
+      <FontAwesomeIcon icon={faRegistered} className="fas fa-lg text-white" />
+      eact
+      </h1>
+
+      <div>
+      <FontAwesomeIcon icon={faShoppingCart} />
+        <span className="">{props.ItemCount} Items</span>
+      </div>
+    </div>
+  );
+}
+
+function DisplayProducts(props) {
+  return (
+    <div>
+      {props.prods.map((product) => {
+        return (
+          <div key={product.id} className="border border-1 p-3">
+            <h4 className="mx-5">{product.desc}</h4>
+            <img
+              src={product.image}
+              width="150"
+              alt={product.desc}
+              className="mx-5"
+            />
+            <button
+              className="btn btn-secondary mx-2"
+              onClick={() => props.increaseBtn(product)}
+            >
+              <FontAwesomeIcon icon={faPlusCircle} className="fas fa-lg" />
+            </button>
+            <button
+              className="btn btn-secondary mx-2"
+              onClick={() => props.decreaseBtn(product)}
+            >
+              <FontAwesomeIcon icon={faMinusCircle} className="fas fa-lg" />
+            </button>
+            <span id="qty" className="mx-5 p-3 border border-3">
+              {product.value}
+            </span>
+            quantity
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
---
+++ shopcart/src/products.js
@@ -0,0 +1,6 @@
+export const products = [
+    { id: 1, image: './products/cologne.jpg', desc: 'Unisex Cologne', ratings: '4', value: 0 },
+    { id: 2, image: './products/iwatch.jpg', desc: 'Apple iWatch', ratings: '3.5', value: 0 },
+    { id: 3, image: './products/mug.jpg', desc: 'Unique Mug', ratings: '5', value: 0 },
+    { id: 4, image: './products/wallet.jpg', desc: 'Mens Wallet', ratings: '4.5', value: 0 }
+]
