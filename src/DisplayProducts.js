a33f1410b8b86207f75ec44b1b8fa1ece9f28d1a
Qi Zhang <zq170829@gmail.com>
2022-01-30 12:21:19 -0600

    price feature added

--- src/DisplayProducts.js
+++ src/DisplayProducts.js
@@ -1,59 +1,76 @@
 import { faPlusCircle, faMinusCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Modal, Button } from "react-bootstrap";
+import { Modal } from "react-bootstrap";
 import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function DisplayProducts(props) {
   const [show, setShow] = useState(false);
-  const [showImge, setShowImge] = useState({});
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   return (
     <div>
-      {props.products.map((product) => {
-        return (
-          <div key={product.id} className="border border-1 p-3">
-            <h4 className="mx-5">{product.desc}</h4>
-            <img
-              onClick={() => handleShow(product)}
-              src={product.image}
-              width="150"
-              alt={product.desc}
-              className="mx-5"
-            />
-            <button
-              className="btn btn-secondary mx-2"
-              onClick={() => props.increaseBtn(product)}
-            >
-              <FontAwesomeIcon icon={faPlusCircle} className="fas fa-lg" />
-            </button>
-            <button
-              className="btn btn-secondary mx-2"
-              onClick={() => props.decreaseBtn(product)}
-            >
-              <FontAwesomeIcon icon={faMinusCircle} className="fas fa-lg" />
-            </button>
-            <span id="qty" className="mx-5 p-3 border border-3">
-              {product.value}
-            </span>
-            quantity
-            <Modal show={show} onHide={handleClose}>
-              <Modal.Header closeButton>
-                <Modal.Title>{product.desc}</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
+        <div className="p-5">
+          <span>Sort Price By: </span>
+          <select>
+            <option>Normal</option>
+            <option>Lowest</option>
+            <option>Highest</option>
+          </select>
+        </div>
+      <div>
+        {props.products.map((product) => {
+          return (
+            <>
+              <div key={product.id} className="border border-1 p-3">
+                <h4 className="mx-5">
+                  {product.desc}
+                  <span className="text-danger mx-3 fs-5">
+                    ${product.price}
+                  </span>
+                </h4>
                 <img
+                  onClick={() => handleShow(product)}
                   src={product.image}
-                  width="350"
+                  width="150"
                   alt={product.desc}
                   className="mx-5"
                 />
-              </Modal.Body>
-              <Modal.Footer>{product.ratings}</Modal.Footer>
-            </Modal>
-          </div>
-        );
-      })}
+                <button
+                  className="btn btn-secondary mx-2"
+                  onClick={() => props.increaseBtn(product)}
+                >
+                  <FontAwesomeIcon icon={faPlusCircle} className="fas fa-lg" />
+                </button>
+                <button
+                  className="btn btn-secondary mx-2"
+                  onClick={() => props.decreaseBtn(product)}
+                >
+                  <FontAwesomeIcon icon={faMinusCircle} className="fas fa-lg" />
+                </button>
+                <span id="qty" className="mx-5 p-3 border border-3">
+                  {product.value}
+                </span>
+                quantity
+              </div>
+              <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title>{product.desc}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <img
+                    src={product.image}
+                    width="350"
+                    alt={product.desc}
+                    className="mx-5"
+                  />
+                  <p>Rating: {product.ratings}</p>
+                </Modal.Body>
+              </Modal>
+            </>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- src/products.js
+++ src/products.js
@@ -1,6 +1,6 @@
 export const products = [
-    { id: 1, image: './products/cologne.jpg', desc: 'Unisex Cologne', ratings: '4', value: 0 },
-    { id: 2, image: './products/iwatch.jpg', desc: 'Apple iWatch', ratings: '3.5', value: 0 },
-    { id: 3, image: './products/mug.jpg', desc: 'Unique Mug', ratings: '5', value: 0 },
-    { id: 4, image: './products/wallet.jpg', desc: 'Mens Wallet', ratings: '4.5', value: 0 }
+    { id: 1, image: './products/cologne.jpg', desc: 'Unisex Cologne', price:'35.00', ratings: '4', value: 0 },
+    { id: 2, image: './products/iwatch.jpg', desc: 'Apple iWatch', price:'199.99', ratings: '3.5', value: 0 },
+    { id: 3, image: './products/mug.jpg', desc: 'Unique Mug', price:'15.00', ratings: '5', value: 0 },
+    { id: 4, image: './products/wallet.jpg', desc: 'Mens Wallet', price:'48.00', ratings: '4.5', value: 0 }
 ]
